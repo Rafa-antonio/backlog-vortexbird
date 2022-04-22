c3c7e175a01ce988fd00e7cb2cfb023c5f4f4e75
1006465657JJDLR <juan.de@uao.edu.co>
2022-04-22 11:55:38 -0500

    Actualizando el script de la creación de la base de datos y añadiendo la documentación correspondiente al proyecto en general

--- database/script_de_creacion.sql
+++ database/script_de_creacion.sql
@@ -1,11 +1,15 @@
 -- Creación de la base de datos
 CREATE DATABASE vortex_bird_db;
 
+-- Se usa la base de datos
+use vortex_bird_db;
+
 -- Creación de la tabla de Analista
-CREATE TABLE ANALISTA(
-    correo VARCHAR(255) NOT NULL,
-    usuario VARCHAR(255),
+CREATE TABLE USUARIO(
+    correo VARCHAR(255) NOT NULL PRIMARY KEY,
+    usuario VARCHAR(255) NULL,
     contrasena VARCHAR(255) NOT NULL,
     nombre VARCHAR(255) NOT NULL,
-    PRIMARY KEY (correo)
+    tipo INT NOT NULL,
 );
+
